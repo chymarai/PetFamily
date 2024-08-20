11959925efdfa3c32d40e709fdba2b289502a316
chymarai <b.rail2012@yandex.ru>
2024-08-20 17:09:49 +0300

    created an animal model

--- backend/src/PetFamily.Domain/Requisites.cs
+++ backend/src/PetFamily.Domain/Requisites.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace PetFamily.Domain
 {
-    internal class Requisites
+    public class Requisite
     {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
     }
 }
