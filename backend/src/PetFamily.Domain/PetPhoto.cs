bd631f30ee9d7e9e593d3c45bf10b95f56468d58
chymarai <b.rail2012@yandex.ru>
2024-08-21 15:56:51 +0300

    AddPetPhoto

---
+++ backend/src/PetFamily.Domain/PetPhoto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFamily.Domain
+{
+    public class PetPhoto
+    {
+        public Guid Id { get; set; }
+        public string Storage { get; private set; } = default!;
+        public bool IsMain { get; private set; }
+    }
+}
