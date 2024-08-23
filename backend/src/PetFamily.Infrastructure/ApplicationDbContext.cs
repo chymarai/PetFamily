ad950c13f5673f1d93814765b550bafc7c86ce2b
chymarai <b.rail2012@yandex.ru>
2024-08-24 01:01:11 +0300

    AddPgAdmin

---
+++ backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetFamily.Domain.Modules;
+
+namespace PetFamily.Infrastructure
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<Volunteer> Modules => Set<Volunteer>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("");
+        }
+    }
+}
