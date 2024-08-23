f332df8bb0accef8a91e7d7f8a0ca657db5a20b8
chymarai <b.rail2012@yandex.ru>
2024-08-24 01:36:26 +0300

    AddDbContext

--- backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
+++ backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using PetFamily.Domain.Modules;
 
 namespace PetFamily.Infrastructure
 {
-    internal class ApplicationDbContext : DbContext
+    internal class ApplicationDbContext(IConfiguration configuration) : DbContext
     {
+        private const string DATABASE = "Database";
         public DbSet<Volunteer> Modules => Set<Volunteer>();
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("");
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString(DATABASE));
+            optionsBuilder.UseUpperSnakeCaseNamingConvention();
+            optionsBuilder.UseLoggerFactory(CreateLoggerFactory());
         }
+
+        private ILoggerFactory CreateLoggerFactory() =>
+            LoggerFactory.Create(builder => { builder.AddConsole(); });
     }
 }
