b3a317458eca867d70d157e98a2a5ac27829e84a
chymarai <157115140+chymarai@users.noreply.github.com>
2024-10-08 13:13:57 +0300

    AddExceptionMiddleware

---
+++ backend/src/PetFamily.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using PetFamily.API.Response;
+using System.Globalization;
+
+namespace PetFamily.API.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context); //вызов всего приложения
+        }
+        catch (Exception ex)
+        {
+            var responseError = new ResponseError("server.internal", ex.Message, null); //формируем ошибку
+            var envelope = Envelope.Error([responseError]);
+
+            context.Response.ContentType = "application/json";  //возвращаем ответ в json
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError; 
+
+            await context.Response.WriteAsJsonAsync(envelope);
+        }
+    }
+}
+
+public static class ExceptionMiddlewareExtensions //
+{
+    public static IApplicationBuilder UseExceptionMiddleware(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionMiddleware>();
+    }
+}
