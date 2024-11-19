8b4448fa579557df465382acbe92a324d4c3e7dc
chymarai <157115140+chymarai@users.noreply.github.com>
2024-11-19 12:28:50 +0300

    AddVerificationBreedAndSpeciesInCreatePetHandler

---
+++ backend/src/PetFamily.Application/DTOs/BreedDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFamily.Application.DTOs;
+public class BreedDto
+{
+    public Guid Id { get; init; }
+    public Guid SpeciesId { get; init; }
+    public string BreedName { get; init; }
+}
