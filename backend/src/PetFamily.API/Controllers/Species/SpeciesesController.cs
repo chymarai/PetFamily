1e04917f19f6e142a4171bd370de25a8631e71cd
chymarai <157115140+chymarai@users.noreply.github.com>
2024-11-21 09:33:00 +0300

    AddDeleteBreedHandlerAddDeleteBreedController

--- backend/src/PetFamily.API/Controllers/Species/SpeciesesController.cs
+++ backend/src/PetFamily.API/Controllers/Species/SpeciesesController.cs
@@ -36,4 +36,18 @@ public class SpeciesesController : ApplicationController
 
         return Ok(result.Value);
     }
+    [HttpDelete("{speciesId:guid}/{breedId:guid}/delete")]
+    public async Task<ActionResult> DeleteBreed(
+    [FromRoute] Guid speciesId,
+    [FromRoute] Guid breedId,
+    [FromServices] DeleteBreedHandler handler,
+    CancellationToken token)
+    {
+        var result = await handler.Handle(new DeleteBreedCommand(speciesId, breedId), token);
+
+        if (result.IsFailure)
+            return result.Error.ToResponse();
+
+        return Ok(result.Value);
+    }
 }
---
+++ backend/src/PetFamily.Application/BreedsManagment/DeleteBreed/DeleteBreedValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFamily.Application.BreedsManagment.DeleteBreed;
+public class DeleteBreedValidator : AbstractValidator<DeleteBreedCommand>
+{
+    public DeleteBreedValidator()
+    {
+        RuleFor(b => b.BreedId).NotEmpty();
+        RuleFor(b => b.SpeciesId).NotEmpty();
+    }
+}
